6db9584ca15a07e6a9d152bc452e0ce81ec5a1bb
Abdul Latheef <abdullatheef@Abduls-MacBook-Pro.local>
2020-09-20 14:06:04 +0500

    show map and pin on order view

--- resources/views/orders/view.blade.php
+++ resources/views/orders/view.blade.php
@@ -31,6 +31,9 @@
               <span class="float-right">{{ $order->customer()->first()->contact }}</span>
             </li>
           </ul>
+          <input type="hidden" name="lat" id="lat" value="{{ $order->customer()->first()->lat }}">
+          <input type="hidden" name="lng" id="lng" value="{{ $order->customer()->first()->lng }}">
+          <div id="map" style="width: 100%; height: 400px;"></div>
         </div>
       </div>
     </div>
@@ -185,7 +188,16 @@ var deliveryUrl = "{{ route('orders.delivery', ['id' => $order->id]) }}";
 var paymentMethodUrl = "{{ route('orders.payment.method', ['id' => $order->id]) }}";
 var paymentStatusUrl = "{{ route('orders.payment.status', ['id' => $order->id]) }}";
 
+var lat;
+var lng;
+
 $(document).ready(function() {
+
+  lat = $('#lat').val();
+  lng = $('#lng').val();
+
+  initMap();
+
   var msg = $('#modalMsg');
 
   $('.delivery-btn').click(function(e) {
@@ -223,6 +235,18 @@ $(document).ready(function() {
 
 });
 
+function initMap() {
+  var male = {lat: 4.1753508, lng: 73.5093263};
+
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 16, center: male});
+  
+  var marker = new google.maps.Marker({
+          position: {lat: parseFloat(lat), lng: parseFloat(lng)},
+          map: map,
+      });
+}
+
 function postUpdate(url, status) {
   $('#confirmModal').modal('show');
   $('#update-confirm-form').attr('action', url);
@@ -235,4 +259,6 @@ function postUpdate(url, status) {
 }
 
 </script>
+
+<script defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDZKgE9_xuf54zKLcUDcqzJKwH0nwXz6RQ"></script>
 @endsection
